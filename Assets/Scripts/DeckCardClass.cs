7c47fa7cc3984774102476486201fea435d618c8
ImIntegrity <sophiecody999@gmail.com>
2024-07-08 23:38:55 -0400

    Episode 1.3
    Improving code, adding and subtracting resource cards, deck card values for giving and taking, removing button from resource cards, game manager values for each resource card held

--- Assets/Scripts/DeckCardClass.cs
+++ Assets/Scripts/DeckCardClass.cs
@@ -7,47 +7,58 @@ using UnityEngine;
 public class DeckCardClass : ScriptableObject
 {
     public string deckCardDescriptionString, deckCardCostString;
-
-    /*
-
-    Make better template, for cost and reusability, redo otherCardMethod so only methods needed are for giving and taking cards
-
-    */
+    public int deckCardTakeInt, deckCardGiveInt; //Action, Knowledge, Strength, Stamina, Money
 
     public void DeckCardMethod()
     {
-        switch (name)
+        GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        if (deckCardTakeInt != 0)
         {
-            case "Get Wife":
-                GetWifeMethod();
-                break;
-
-            case "Pay Taxes":
-                PayTaxeseMethod();
-                break;
-
-            case "Rich Family":
-                RichFamilyMethod();
-                break;
-
-            default:
-                Debug.Log("Default");
-                break;
+            if ((deckCardTakeInt / (int)Mathf.Pow(10, 0)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Money", (deckCardTakeInt / (int)Mathf.Pow(10, 0)) % 10, false);
+            }
+            if ((deckCardTakeInt / (int)Mathf.Pow(10, 1)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Stamina", (deckCardTakeInt / (int)Mathf.Pow(10, 1)) % 10, false);
+            }
+            if ((deckCardTakeInt / (int)Mathf.Pow(10, 2)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Strength", (deckCardTakeInt / (int)Mathf.Pow(10, 2)) % 10, false);
+            }
+            if ((deckCardTakeInt / (int)Mathf.Pow(10, 3)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Knowledge", (deckCardTakeInt / (int)Mathf.Pow(10, 3)) % 10, false);
+            }
+            if ((deckCardTakeInt / (int)Mathf.Pow(10, 4)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Action", (deckCardTakeInt / (int)Mathf.Pow(10, 4)) % 10, false);
+            }
         }
-    }
 
-    private void GetWifeMethod()
-    {
-        Debug.Log("Wife XD");
-    }
-
-    private void PayTaxeseMethod()
-    {
-        Debug.Log("Taxes yay");
-    }
-
-    private void RichFamilyMethod()
-    {
-        Debug.Log("Chose rich family, -2 Money Cards");
+        if (deckCardGiveInt != 0)
+        {
+            if ((deckCardGiveInt / (int)Mathf.Pow(10, 0)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Money", (deckCardGiveInt / (int)Mathf.Pow(10, 0)) % 10, true);
+            }
+            if ((deckCardGiveInt / (int)Mathf.Pow(10, 1)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Stamina", (deckCardGiveInt / (int)Mathf.Pow(10, 1)) % 10, true);
+            }
+            if ((deckCardGiveInt / (int)Mathf.Pow(10, 2)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Strength", (deckCardGiveInt / (int)Mathf.Pow(10, 2)) % 10, true);
+            }
+            if ((deckCardGiveInt / (int)Mathf.Pow(10, 3)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Knowledge", (deckCardGiveInt / (int)Mathf.Pow(10, 3)) % 10, true);
+            }
+            if ((deckCardGiveInt / (int)Mathf.Pow(10, 4)) % 10 != 0)
+            {
+                gameManager.GiveOrTakeResourceCard("Action", (deckCardGiveInt / (int)Mathf.Pow(10, 4)) % 10, true);
+            }
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,10 @@ public class GameManager : MonoBehaviour
 {
     public GameObject deckCardPrefab, resourceCardPrefab;
     public Transform deckHand, playerHand;
-
     public List<DeckCardClass> deckStarterCards;
     public List<ResourceCardClass> resourceStarterCards;
+
+    public int moneyCardsInt = 0, staminaCardsInt = 0, strengthCardsInt = 0, knowledgeCardsInt = 0, actionCardsInt = 0;
 
     void Start()
     {
@@ -37,6 +38,58 @@ public class GameManager : MonoBehaviour
             clone.GetComponent<ResourceCardDisplay>().resourceCard = resourceStarterCards[i];
             clone.transform.localPosition = new Vector2(-600 + (i * 400), -300);
             clone.name = clone.GetComponent<ResourceCardDisplay>().resourceCard.name;
+
+            GiveOrTakeResourceCard(clone.name, 1, true);
+        }
+    }
+
+    public void GiveOrTakeResourceCard(string card, int num, bool isGiving)
+    {
+        if (isGiving)
+        {
+            if (card == "Money")
+            {
+                moneyCardsInt += num;
+            }
+            else if (card == "Stamina")
+            {
+                staminaCardsInt += num;
+            }
+            else if (card == "Strength")
+            {
+                strengthCardsInt += num;
+            }
+            else if (card == "Knowledge")
+            {
+                knowledgeCardsInt += num;
+            }
+            else if (card == "Action")
+            {
+                actionCardsInt += num;
+            }
+        }
+        else
+        {
+            if (card == "Money")
+            {
+                moneyCardsInt -= num;
+            }
+            else if (card == "Stamina")
+            {
+                staminaCardsInt -= num;
+            }
+            else if (card == "Strength")
+            {
+                strengthCardsInt -= num;
+            }
+            else if (card == "Knowledge")
+            {
+                knowledgeCardsInt -= num;
+            }
+            else if (card == "Action")
+            {
+                actionCardsInt -= num;
+            }
         }
     }
 }
